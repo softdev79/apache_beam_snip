67bfe9852a0075b0fd8d37f435e5290f2305c0f5
sofdev79 <vishalsoftdev79@gmail.com>
2024-05-25 03:12:43 +0530

    Update JoinTable.java

--- src/main/java/JoinTable.java
+++ src/main/java/JoinTable.java
@@ -1,9 +1,11 @@
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.beam.runners.dataflow.DataflowRunner;
 import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 import com.google.api.services.bigquery.model.TableRow;
 import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.kafka.KafkaIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.options.ValueProvider;
@@ -15,10 +17,13 @@ import org.apache.beam.sdk.values.*;
 import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.transforms.Filter;
 import org.apache.beam.sdk.extensions.sql.SqlTransform;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.beam.sdk.options.ValueProvider;
+import org.apache.kafka.common.serialization.StringSerializer;
 
-
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class JoinTable {
@@ -28,12 +33,12 @@ public class JoinTable {
         options.setRunner(DataflowRunner.class); // Set the runner to DataflowRunner
         options.setProject("steady-burner-336411");
         options.setRegion("us-east1");
-        //options.setGcpTempLocation("gs://dataflow_statging/tmp");
+        //options.setGcpTempLocation("gs://dataflow-apache-quickstart_steady-burner-336411/temp/");
         //options.setStagingLocation("gs://dataflow_statging/stg");
         options.setJobName("join-bigquery-tables");
         Pipeline p = Pipeline.create(options);
 
-        String query = "insert into `steady-burner-336411.vishal_dataset.output_table select name , d.dept_nm , emp_id from steady-burner-336411.vishal_dataset.employee e inner join steady-burner-336411.vishal_dataset.department d on e.dep_id=d.dept_id";
+        /*String query = "insert into `steady-burner-336411.vishal_dataset.output_table select name , d.dept_nm , emp_id from steady-burner-336411.vishal_dataset.employee e inner join steady-burner-336411.vishal_dataset.department d on e.dep_id=d.dept_id";
 
         PCollection<TableRow> join=p.apply("Read from BQ query",BigQueryIO.readTableRows()
                 .fromQuery("select name , d.dept_nm , emp_id from `steady-burner-336411.vishal_dataset.employee` e inner join steady-burner-336411.vishal_dataset.department d on e.dep_id=d.dept_id ;")
@@ -45,8 +50,38 @@ public class JoinTable {
                 .to("steady-burner-336411.vishal_dataset.output_table")
                 .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED)
                 .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_TRUNCATE)
-                .withSchema(BiquerySchema.getOutputSchema()));
+                .withSchema(BiquerySchema.getOutputSchema())); */
+        Map<String, Object> kafkaConfig = new HashMap<>();
+        kafkaConfig.put("bootstrap.servers", "pkc-619z3.us-east1.gcp.confluent.cloud:9092");
+        kafkaConfig.put("security.protocol", "SASL_SSL");
+        kafkaConfig.put("sasl.mechanism", "PLAIN");
+        kafkaConfig.put("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username='AVVANESSOLCAIQ3K' password='1e3XJRF0mUhVYa2/binldvCFwk1euwNLIA3laQ0TqsIgKv9rglmVlTj+eYb7Sk9W';");
+        kafkaConfig.put("ssl.endpoint.identification.algorithm", "https");
+        kafkaConfig.put("client.dns.lookup", "use_all_dns_ips");
+        kafkaConfig.put("request.timeout.ms", "30000");
+        kafkaConfig.put("retry.backoff.ms", "500");
+        ObjectMapper mapper = new ObjectMapper();
+        p.apply("ReadFromBigQuery", BigQueryIO.readTableRows()
+                        .from("steady-burner-336411.vishal_dataset.employee"))
+                .apply("ConvertToJson", MapElements.into(TypeDescriptor.of(String.class))
+                        .via((TableRow row) -> {
+                            try {
+                                // Convert TableRow to JSON string
+                                return mapper.writeValueAsString(row);
+                            } catch (Exception e) {
+                                throw new RuntimeException("Error converting TableRow to JSON", e);
+                            }
+                        }))
+                .apply("WriteToKafka", KafkaIO.<String, String>write()
+                        .withBootstrapServers("pkc-619z3.us-east1.gcp.confluent.cloud:9092")
+                        .withTopic("bq_to_kafka")
+                        .withKeySerializer(StringSerializer.class)
+                        .withValueSerializer(StringSerializer.class)
+                        .withProducerConfigUpdates(kafkaConfig)
+                        .values());
+
         p.run().waitUntilFinish();
+
     }
 }
 
